9e25aa9024c094a032b413a9a5e0705aa1e16d43
antonio-rosales-tfs <antonio.rosales@thermofisher.com>
2024-02-05 20:23:38 -0800

    https url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ import { environmentEnum } from 'src/app/global/tools';
 
 export const environment = {
     environment: environmentEnum.production,
-    apiUrl: 'http://208.109.188.104:3000/',
+    apiUrl: 'https://208.109.188.104:3000/',
     fileFolder: '/Users/antonio.rosales/files/development'
 };
