79b69d678c06816b63d88e5437e9e1b9788f36b5
antonio-rosales-tfs <antonio.rosales@thermofisher.com>
2024-02-11 19:56:34 -0800

    change into apiUrl production enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ import { environmentEnum } from 'src/app/global/tools';
 
 export const environment = {
     environment: environmentEnum.production,
-    apiUrl: 'https://208.109.188.104:3000/',
+    apiUrl: 'https://portal.atdtools.com.mx/',
     fileFolder: '/Users/antonio.rosales/files/development'
 };
